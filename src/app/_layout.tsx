6a56be1817d82de76d278d82b1609ca901769c5f
primonmari <marisprimoon@gmail.com>
2024-12-22 15:44:51 -0300

    first-commit

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -27,7 +27,7 @@ export default function Layout() {
       <Stack
         screenOptions={{
           headerShown: false,
-          contentStyle: { backgroundColor: colors.gray[100] },
+          contentStyle: { backgroundColor: colors.gray[100] },  
         }}
       />
     </GestureHandlerRootView>
